828efb7e4144a25c7ff64853104e6822fc4fd83a
Szabo Marton <mart.szaboo@gmail.com>
2025-07-18 14:05:16 +0200

    Only dist

--- quiz-app/src/pages/MainPage.jsx
+++
@@ -1,9 +0,0 @@
-const MainPage = ({ children }) => {
-  return (
-    <div className="container text-bg-light min-vh-100 align-items-center">
-      {children}
-    </div>
-  );
-};
-
-export default MainPage;
